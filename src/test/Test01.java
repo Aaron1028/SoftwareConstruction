1df18a4ba42fa90ab81166e3613e3f74ba07c7f0
Aaron1028 <3459976576@qq.com>
2019-11-11 21:52:22 +0800

    2019/11/11 test01-2 未实现模块化

--- src/test/Test01.java
+++ src/test/Test01.java
@@ -7,22 +7,33 @@ public class Test01 {
 	private static int right_operand = 0;
 	private static char operator = '+';
 	private static int value = 0;
+	static int[][] arr = new int[50][3];
 	public static void main(String[] args) {
 		Random random = new Random();
 		for (int i = 0; i < 50; i++) {
-			int op;
-			left_operand = random.nextInt(UPPER + 1);
-			right_operand = random.nextInt(UPPER + 1);
-			op = random.nextInt(2);
-			if (op == 1) {
-				operator = '+';
-				value = left_operand + right_operand;
-			} else {
-				operator = '-';
-				value = left_operand - right_operand;
+			do {
+				int op;
+				left_operand = random.nextInt(UPPER + 1);
+				right_operand = random.nextInt(UPPER + 1);
+				op = random.nextInt(2);
+				if (op == 1) {
+					operator = '+';
+					value = left_operand + right_operand;
+				} else {
+					operator = '-';
+					value = left_operand - right_operand;
+				}
+				arr[i][0] = left_operand;
+				arr[i][1] = right_operand;
+				arr[i][2] = op;
+			} while (value >= 100 || value <= 0);
+
+			if (i % 5 == 0) {
+				System.out.print("\n");
+				System.out.print((i + 1) + "~" + (i + 5) + ":");
 			}
-			System.out.println("" + (i + 1) + ":\t" + left_operand + operator
-					+ right_operand + "=");
+			System.out.print(
+					"\t" + left_operand + operator + right_operand + "=");
 		}
 	}
 }
